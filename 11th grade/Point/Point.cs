935638081344351dc68ce3708630d65a68ddc7d2
Niagara Georgieva <niagara.georgieva@gmail.com>
2021-01-06 21:55:00 +0200

    Add method to check if 3 points are on 1 line.

--- 11th grade/Point/Point.cs
+++ 11th grade/Point/Point.cs
@@ -49,6 +49,15 @@ namespace ConsoleApp1
             }
             return location;
         }
+        public void InLineWith(Point A, Point B)
+        {
+            double x1 = this.x - A.x;
+            double y1 = this.y - A.y;
+            double x2 = this.x - B.x;
+            double y2 = this.y - B.y;
+            if(Math.Round(x1/x2, 3) == Math.Round(y1 / y2, 3)) { Console.WriteLine("C лежи на правата, минаваща през A и B."); }
+            else { Console.WriteLine("C не лежи на правата, минаваща през A и B."); }
+        }
     }
 
     class Program
@@ -56,24 +65,29 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Въведете координатите на A: ");
-            double x1 = double.Parse(Console.ReadLine());
-            double y1 = double.Parse(Console.ReadLine());
-            Point A = new Point(x1, y1);
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            Point A = new Point(x, y);
             Console.WriteLine("Въведете координатите на B: ");
-            double x2 = double.Parse(Console.ReadLine());
-            double y2 = double.Parse(Console.ReadLine());
-            Point B = new Point(x2, y2);
-            Point C = new Point(A);
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            Point B = new Point(x, y);
+            Point A2 = new Point(A);
             if (A.CheckIfMatch(B)) { Console.WriteLine("A и B съвпадат."); }
             else { Console.WriteLine("Разстоянието между A и B е: " + A.Dist(B)); }
-            if (C.CheckIfMatch(B)) { Console.WriteLine("C и B съвпадат."); }
-            else { Console.WriteLine("Разстоянието между C и B е: " + C.Dist(B)); }
+            if (A2.CheckIfMatch(B)) { Console.WriteLine("A2 и B съвпадат."); }
+            else { Console.WriteLine("Разстоянието между A2 и B е: " + A2.Dist(B)); }
             Point M = A.Middle(B);
             Console.WriteLine("M(" + M.X + ";" + M.Y + ")");
             Console.WriteLine("Точката е разположена " + M.Quadrant());           
             Point N = A.SymmetricTo(M);
             Console.WriteLine("N(" + N.X + ";" + N.Y + ")");
             Console.WriteLine("Точката е разположена " + N.Quadrant());
+            Console.WriteLine("Въведете координатите на C: ");
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            Point C = new Point(x, y);
+            C.InLineWith(A, B);
         }
     }
 }
