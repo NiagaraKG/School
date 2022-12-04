c99c5a88c74ee818e3ebdb56f50a884b55fbb974
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-12-04 22:18:38 +0200

    Add files via upload

---
+++ 11th grade/Module 1 - Object-oriented design and programming/Visual programming/27.01.2021 - clasResults/27.01.2021 - clasResults/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _27._01._2021___clasResults
+{
+    public partial class Form1 : Form
+    {
+        public class Student
+        {
+            public string clas;
+            public string ID;
+            public string name;
+            private double bulgarian, foreign, math, physics, chemistry, biology, average;
+
+            public double Bulgarian
+            {
+                get { return this.bulgarian; }
+                set
+                {
+                    if (value >= 2.00 && value <= 6.00) { this.bulgarian = value; }
+                    else { Console.WriteLine("Невалидна оценка"); }
+                }
+            }
+            public double Foreign
+            {
+                get { return this.foreign; }
+                set
+                {
+                    if (value >= 2.00 && value <= 6.00) { this.foreign = value; }
+                    else { Console.WriteLine("Невалидна оценка"); }
+                }
+            }
+            public double Math
+            {
+                get { return this.math; }
+                set
+                {
+                    if (value >= 2.00 && value <= 6.00) { this.math = value; }
+                    else { Console.WriteLine("Невалидна оценка"); }
+                }
+            }
+            public double Physics
+            {
+                get { return this.physics; }
+                set
+                {
+                    if (value >= 2.00 && value <= 6.00) { this.physics = value; }
+                    else { Console.WriteLine("Невалидна оценка"); }
+                }
+            }
+            public double Chemistry
+            {
+                get { return this.chemistry; }
+                set
+                {
+                    if (value >= 2.00 && value <= 6.00) { this.chemistry = value; }
+                    else { Console.WriteLine("Невалидна оценка"); }
+                }
+            }
+            public double Biology
+            {
+                get { return this.biology; }
+                set
+                {
+                    if (value >= 2.00 && value <= 6.00) { this.biology = value; }
+                    else { Console.WriteLine("Невалидна оценка"); }
+                }
+            }
+            public double Average
+            {
+                get { return this.average; }
+                set
+                {
+                    if (value >= 2.00 && value <= 6.00) { this.average = value; }
+                    else { Console.WriteLine("Невалидна оценка"); }
+                }
+            }
+        }
+
+        Student currentStudent = new Student();
+        Student[] clas = new Student[27];
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 26; i++) { clas[i] = new Student(); }
+        }
+
+        private void buttonClean_Click(object sender, EventArgs e)
+        {
+            textBoxClas.Text = "";
+            textBoxID.Text = "";
+            textBoxName.Text = "";
+            textBoxBulgarian.Text = "";
+            textBoxForeign.Text = "";
+            textBoxMath.Text = "";
+            textBoxPhysics.Text = "";
+            textBoxChemistry.Text = "";
+            textBoxBiology.Text = "";
+            labelAverageGrade.Text = "";
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBoxID.Text);
+            double average;
+            if(i>=1 && i <= 26)
+            {
+                clas[i].clas = textBoxClas.Text;
+                clas[i].ID = textBoxID.Text;
+                clas[i].name = textBoxName.Text;
+                clas[i].Bulgarian = double.Parse(textBoxBulgarian.Text);
+                clas[i].Foreign = double.Parse(textBoxForeign.Text);
+                clas[i].Math = double.Parse(textBoxMath.Text);
+                clas[i].Physics = double.Parse(textBoxPhysics.Text);
+                clas[i].Chemistry = double.Parse(textBoxChemistry.Text);
+                clas[i].Biology = double.Parse(textBoxBiology.Text);
+                average = (double.Parse(textBoxBulgarian.Text) + double.Parse(textBoxForeign.Text) + double.Parse(textBoxMath.Text) + double.Parse(textBoxPhysics.Text) + double.Parse(textBoxChemistry.Text) + double.Parse(textBoxBiology.Text)) / 6;
+                clas[i].Average = average;
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBoxID.Text);           
+            if (i >= 1 && i <= 26)
+            {
+                textBoxClas.Text = clas[i].clas;
+                textBoxName.Text = clas[i].name;
+                textBoxBulgarian.Text = clas[i].Bulgarian.ToString();
+                textBoxForeign.Text = clas[i].Foreign.ToString();
+                textBoxMath.Text = clas[i].Math.ToString();
+                textBoxPhysics.Text = clas[i].Physics.ToString();
+                textBoxChemistry.Text = clas[i].Chemistry.ToString();
+                textBoxBiology.Text = clas[i].Biology.ToString();
+                labelAverageGrade.Text = clas[i].Average.ToString("0.00");
+            }
+        }
+
+        private void buttonAverage_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBoxID.Text);
+            labelAverageGrade.Text = clas[i].Average.ToString("0.00");
+        }
+
+        private void buttonSaveFile_Click(object sender, EventArgs e)
+        {
+            string s;
+            StreamWriter writer = new StreamWriter("C:/Users/Public/9aClas.txt",false);
+            for (int i = 1; i <= 26; i++)
+            {
+                s = clas[i].clas + " " + clas[i].ID + " " + clas[i].name + " " + clas[i].Bulgarian + " " + clas[i].Foreign + " " + clas[i].Math + " " + clas[i].Physics + " " + clas[i].Chemistry + " " + clas[i].Biology + " " + clas[i].Average;
+                writer.WriteLine(s);
+            }
+            writer.Close();
+        }
+    }
+}
