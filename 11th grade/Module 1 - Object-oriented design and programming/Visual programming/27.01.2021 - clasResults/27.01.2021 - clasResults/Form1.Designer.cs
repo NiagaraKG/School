61fb0366e16683c159fc568105960832f7c70ca1
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-12-05 23:34:26 +0200

    Add files via upload

---
+++ 11th grade/Module 1 - Object-oriented design and programming/Visual programming/27.01.2021 - clasResults/27.01.2021 - clasResults/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace _27._01._2021___clasResults
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelClas = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelBulgarian = new System.Windows.Forms.Label();
+            this.labelForeign = new System.Windows.Forms.Label();
+            this.labelMath = new System.Windows.Forms.Label();
+            this.labelPhysics = new System.Windows.Forms.Label();
+            this.labelChemistry = new System.Windows.Forms.Label();
+            this.labelBiology = new System.Windows.Forms.Label();
+            this.labelAverageText = new System.Windows.Forms.Label();
+            this.labelAverageGrade = new System.Windows.Forms.Label();
+            this.labelFile = new System.Windows.Forms.Label();
+            this.textBoxClas = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxBulgarian = new System.Windows.Forms.TextBox();
+            this.textBoxForeign = new System.Windows.Forms.TextBox();
+            this.textBoxMath = new System.Windows.Forms.TextBox();
+            this.textBoxPhysics = new System.Windows.Forms.TextBox();
+            this.textBoxChemistry = new System.Windows.Forms.TextBox();
+            this.textBoxBiology = new System.Windows.Forms.TextBox();
+            this.buttonClean = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonAverage = new System.Windows.Forms.Button();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelClas
+            // 
+            this.labelClas.AutoSize = true;
+            this.labelClas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelClas.Location = new System.Drawing.Point(23, 25);
+            this.labelClas.Name = "labelClas";
+            this.labelClas.Size = new System.Drawing.Size(56, 25);
+            this.labelClas.TabIndex = 0;
+            this.labelClas.Text = "Клас";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelID.Location = new System.Drawing.Point(180, 25);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(31, 25);
+            this.labelID.TabIndex = 1;
+            this.labelID.Text = "№";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelName.Location = new System.Drawing.Point(317, 25);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(54, 25);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Име";
+            // 
+            // labelBulgarian
+            // 
+            this.labelBulgarian.AutoSize = true;
+            this.labelBulgarian.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelBulgarian.Location = new System.Drawing.Point(23, 93);
+            this.labelBulgarian.Name = "labelBulgarian";
+            this.labelBulgarian.Size = new System.Drawing.Size(52, 25);
+            this.labelBulgarian.TabIndex = 3;
+            this.labelBulgarian.Text = "БЕЛ";
+            // 
+            // labelForeign
+            // 
+            this.labelForeign.AutoSize = true;
+            this.labelForeign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelForeign.Location = new System.Drawing.Point(23, 143);
+            this.labelForeign.Name = "labelForeign";
+            this.labelForeign.Size = new System.Drawing.Size(111, 25);
+            this.labelForeign.TabIndex = 4;
+            this.labelForeign.Text = "Чужд език";
+            // 
+            // labelMath
+            // 
+            this.labelMath.AutoSize = true;
+            this.labelMath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelMath.Location = new System.Drawing.Point(23, 191);
+            this.labelMath.Name = "labelMath";
+            this.labelMath.Size = new System.Drawing.Size(131, 25);
+            this.labelMath.TabIndex = 5;
+            this.labelMath.Text = "Математика";
+            // 
+            // labelPhysics
+            // 
+            this.labelPhysics.AutoSize = true;
+            this.labelPhysics.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelPhysics.Location = new System.Drawing.Point(273, 93);
+            this.labelPhysics.Name = "labelPhysics";
+            this.labelPhysics.Size = new System.Drawing.Size(84, 25);
+            this.labelPhysics.TabIndex = 6;
+            this.labelPhysics.Text = "Физика";
+            // 
+            // labelChemistry
+            // 
+            this.labelChemistry.AutoSize = true;
+            this.labelChemistry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelChemistry.Location = new System.Drawing.Point(273, 143);
+            this.labelChemistry.Name = "labelChemistry";
+            this.labelChemistry.Size = new System.Drawing.Size(76, 25);
+            this.labelChemistry.TabIndex = 7;
+            this.labelChemistry.Text = "Химия";
+            // 
+            // labelBiology
+            // 
+            this.labelBiology.AutoSize = true;
+            this.labelBiology.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelBiology.Location = new System.Drawing.Point(273, 191);
+            this.labelBiology.Name = "labelBiology";
+            this.labelBiology.Size = new System.Drawing.Size(99, 25);
+            this.labelBiology.TabIndex = 8;
+            this.labelBiology.Text = "Биология";
+            // 
+            // labelAverageText
+            // 
+            this.labelAverageText.AutoSize = true;
+            this.labelAverageText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelAverageText.Location = new System.Drawing.Point(23, 258);
+            this.labelAverageText.Name = "labelAverageText";
+            this.labelAverageText.Size = new System.Drawing.Size(137, 25);
+            this.labelAverageText.TabIndex = 9;
+            this.labelAverageText.Text = "Среден успех";
+            // 
+            // labelAverageGrade
+            // 
+            this.labelAverageGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAverageGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelAverageGrade.Location = new System.Drawing.Point(178, 255);
+            this.labelAverageGrade.Name = "labelAverageGrade";
+            this.labelAverageGrade.Size = new System.Drawing.Size(56, 30);
+            this.labelAverageGrade.TabIndex = 10;
+            // 
+            // labelFile
+            // 
+            this.labelFile.AutoSize = true;
+            this.labelFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelFile.Location = new System.Drawing.Point(23, 315);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(70, 25);
+            this.labelFile.TabIndex = 11;
+            this.labelFile.Text = "Файл:";
+            // 
+            // textBoxClas
+            // 
+            this.textBoxClas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxClas.Location = new System.Drawing.Point(85, 25);
+            this.textBoxClas.Name = "textBoxClas";
+            this.textBoxClas.Size = new System.Drawing.Size(56, 30);
+            this.textBoxClas.TabIndex = 12;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxID.Location = new System.Drawing.Point(219, 25);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(56, 30);
+            this.textBoxID.TabIndex = 13;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxName.Location = new System.Drawing.Point(384, 25);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(256, 30);
+            this.textBoxName.TabIndex = 14;
+            // 
+            // textBoxBulgarian
+            // 
+            this.textBoxBulgarian.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxBulgarian.Location = new System.Drawing.Point(178, 90);
+            this.textBoxBulgarian.Name = "textBoxBulgarian";
+            this.textBoxBulgarian.Size = new System.Drawing.Size(56, 30);
+            this.textBoxBulgarian.TabIndex = 15;
+            // 
+            // textBoxForeign
+            // 
+            this.textBoxForeign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxForeign.Location = new System.Drawing.Point(178, 140);
+            this.textBoxForeign.Name = "textBoxForeign";
+            this.textBoxForeign.Size = new System.Drawing.Size(56, 30);
+            this.textBoxForeign.TabIndex = 16;
+            // 
+            // textBoxMath
+            // 
+            this.textBoxMath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxMath.Location = new System.Drawing.Point(178, 188);
+            this.textBoxMath.Name = "textBoxMath";
+            this.textBoxMath.Size = new System.Drawing.Size(56, 30);
+            this.textBoxMath.TabIndex = 17;
+            // 
+            // textBoxPhysics
+            // 
+            this.textBoxPhysics.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxPhysics.Location = new System.Drawing.Point(384, 90);
+            this.textBoxPhysics.Name = "textBoxPhysics";
+            this.textBoxPhysics.Size = new System.Drawing.Size(56, 30);
+            this.textBoxPhysics.TabIndex = 18;
+            // 
+            // textBoxChemistry
+            // 
+            this.textBoxChemistry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxChemistry.Location = new System.Drawing.Point(384, 140);
+            this.textBoxChemistry.Name = "textBoxChemistry";
+            this.textBoxChemistry.Size = new System.Drawing.Size(56, 30);
+            this.textBoxChemistry.TabIndex = 19;
+            // 
+            // textBoxBiology
+            // 
+            this.textBoxBiology.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxBiology.Location = new System.Drawing.Point(384, 188);
+            this.textBoxBiology.Name = "textBoxBiology";
+            this.textBoxBiology.Size = new System.Drawing.Size(56, 30);
+            this.textBoxBiology.TabIndex = 20;
+            // 
+            // buttonClean
+            // 
+            this.buttonClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonClean.Location = new System.Drawing.Point(492, 89);
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.Size = new System.Drawing.Size(148, 33);
+            this.buttonClean.TabIndex = 21;
+            this.buttonClean.Text = "Изчистване";
+            this.buttonClean.UseVisualStyleBackColor = true;
+            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonSave.Location = new System.Drawing.Point(492, 139);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(148, 33);
+            this.buttonSave.TabIndex = 22;
+            this.buttonSave.Text = "Запазване";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonSearch.Location = new System.Drawing.Point(492, 187);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(148, 33);
+            this.buttonSearch.TabIndex = 23;
+            this.buttonSearch.Text = "Търсене";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonAverage
+            // 
+            this.buttonAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonAverage.Location = new System.Drawing.Point(278, 255);
+            this.buttonAverage.Name = "buttonAverage";
+            this.buttonAverage.Size = new System.Drawing.Size(162, 33);
+            this.buttonAverage.TabIndex = 24;
+            this.buttonAverage.Text = "Среден успех";
+            this.buttonAverage.UseVisualStyleBackColor = true;
+            this.buttonAverage.Click += new System.EventHandler(this.buttonAverage_Click);
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonSaveFile.Location = new System.Drawing.Point(98, 311);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(236, 33);
+            this.buttonSaveFile.TabIndex = 25;
+            this.buttonSaveFile.Text = "Запазване на файла";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 366);
+            this.Controls.Add(this.buttonSaveFile);
+            this.Controls.Add(this.buttonAverage);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonClean);
+            this.Controls.Add(this.textBoxBiology);
+            this.Controls.Add(this.textBoxChemistry);
+            this.Controls.Add(this.textBoxPhysics);
+            this.Controls.Add(this.textBoxMath);
+            this.Controls.Add(this.textBoxForeign);
+            this.Controls.Add(this.textBoxBulgarian);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.textBoxClas);
+            this.Controls.Add(this.labelFile);
+            this.Controls.Add(this.labelAverageGrade);
+            this.Controls.Add(this.labelAverageText);
+            this.Controls.Add(this.labelBiology);
+            this.Controls.Add(this.labelChemistry);
+            this.Controls.Add(this.labelPhysics);
+            this.Controls.Add(this.labelMath);
+            this.Controls.Add(this.labelForeign);
+            this.Controls.Add(this.labelBulgarian);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelClas);
+            this.Name = "Form1";
+            this.Text = "clas Results";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelClas;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelBulgarian;
+        private System.Windows.Forms.Label labelForeign;
+        private System.Windows.Forms.Label labelMath;
+        private System.Windows.Forms.Label labelPhysics;
+        private System.Windows.Forms.Label labelChemistry;
+        private System.Windows.Forms.Label labelBiology;
+        private System.Windows.Forms.Label labelAverageText;
+        private System.Windows.Forms.Label labelAverageGrade;
+        private System.Windows.Forms.Label labelFile;
+        private System.Windows.Forms.TextBox textBoxClas;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxBulgarian;
+        private System.Windows.Forms.TextBox textBoxForeign;
+        private System.Windows.Forms.TextBox textBoxMath;
+        private System.Windows.Forms.TextBox textBoxPhysics;
+        private System.Windows.Forms.TextBox textBoxChemistry;
+        private System.Windows.Forms.TextBox textBoxBiology;
+        private System.Windows.Forms.Button buttonClean;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonAverage;
+        private System.Windows.Forms.Button buttonSaveFile;
+    }
+}
+
