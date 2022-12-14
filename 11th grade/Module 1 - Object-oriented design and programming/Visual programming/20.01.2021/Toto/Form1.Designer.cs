d3c352fa9c3cdcf42ff62ddb8e84ebdc4bc886a4
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-12-14 23:24:47 +0200

    Add files via upload

---
+++ 11th grade/Module 1 - Object-oriented design and programming/Visual programming/20.01.2021/Toto/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Toto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text1 = new System.Windows.Forms.Label();
+            this.number1 = new System.Windows.Forms.Label();
+            this.text2 = new System.Windows.Forms.Label();
+            this.number2 = new System.Windows.Forms.Label();
+            this.text3 = new System.Windows.Forms.Label();
+            this.number3 = new System.Windows.Forms.Label();
+            this.text4 = new System.Windows.Forms.Label();
+            this.number4 = new System.Windows.Forms.Label();
+            this.text5 = new System.Windows.Forms.Label();
+            this.number5 = new System.Windows.Forms.Label();
+            this.text6 = new System.Windows.Forms.Label();
+            this.number6 = new System.Windows.Forms.Label();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            this.text1.AutoSize = true;
+            this.text1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.text1.Location = new System.Drawing.Point(31, 43);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(161, 29);
+            this.text1.TabIndex = 0;
+            this.text1.Text = "Първо число";
+            // 
+            // number1
+            // 
+            this.number1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.number1.Location = new System.Drawing.Point(198, 42);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(52, 38);
+            this.number1.TabIndex = 1;
+            // 
+            // text2
+            // 
+            this.text2.AutoSize = true;
+            this.text2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.text2.Location = new System.Drawing.Point(31, 103);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(159, 29);
+            this.text2.TabIndex = 2;
+            this.text2.Text = "Второ число";
+            // 
+            // number2
+            // 
+            this.number2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.number2.Location = new System.Drawing.Point(198, 102);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(52, 38);
+            this.number2.TabIndex = 3;
+            // 
+            // text3
+            // 
+            this.text3.AutoSize = true;
+            this.text3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.text3.Location = new System.Drawing.Point(31, 163);
+            this.text3.Name = "text3";
+            this.text3.Size = new System.Drawing.Size(159, 29);
+            this.text3.TabIndex = 4;
+            this.text3.Text = "Трето число";
+            // 
+            // number3
+            // 
+            this.number3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.number3.Location = new System.Drawing.Point(198, 162);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(52, 38);
+            this.number3.TabIndex = 5;
+            // 
+            // text4
+            // 
+            this.text4.AutoSize = true;
+            this.text4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.text4.Location = new System.Drawing.Point(313, 43);
+            this.text4.Name = "text4";
+            this.text4.Size = new System.Drawing.Size(199, 29);
+            this.text4.TabIndex = 6;
+            this.text4.Text = "Четвърто число";
+            // 
+            // number4
+            // 
+            this.number4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.number4.Location = new System.Drawing.Point(518, 43);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(52, 38);
+            this.number4.TabIndex = 7;
+            // 
+            // text5
+            // 
+            this.text5.AutoSize = true;
+            this.text5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.text5.Location = new System.Drawing.Point(313, 103);
+            this.text5.Name = "text5";
+            this.text5.Size = new System.Drawing.Size(146, 29);
+            this.text5.TabIndex = 8;
+            this.text5.Text = "Пето число";
+            // 
+            // number5
+            // 
+            this.number5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.number5.Location = new System.Drawing.Point(518, 102);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(52, 38);
+            this.number5.TabIndex = 9;
+            // 
+            // text6
+            // 
+            this.text6.AutoSize = true;
+            this.text6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.text6.Location = new System.Drawing.Point(313, 163);
+            this.text6.Name = "text6";
+            this.text6.Size = new System.Drawing.Size(161, 29);
+            this.text6.TabIndex = 10;
+            this.text6.Text = "Шесто число";
+            // 
+            // number6
+            // 
+            this.number6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.number6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.number6.Location = new System.Drawing.Point(518, 162);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(52, 38);
+            this.number6.TabIndex = 11;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.buttonGenerate.Location = new System.Drawing.Point(216, 228);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(160, 50);
+            this.buttonGenerate.TabIndex = 12;
+            this.buttonGenerate.Text = "Генерирай";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 301);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.number6);
+            this.Controls.Add(this.text6);
+            this.Controls.Add(this.number5);
+            this.Controls.Add(this.text5);
+            this.Controls.Add(this.number4);
+            this.Controls.Add(this.text4);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.text3);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.text1);
+            this.Name = "Form1";
+            this.Text = "Тото 6 от 49";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label text1;
+        private System.Windows.Forms.Label number1;
+        private System.Windows.Forms.Label text2;
+        private System.Windows.Forms.Label number2;
+        private System.Windows.Forms.Label text3;
+        private System.Windows.Forms.Label number3;
+        private System.Windows.Forms.Label text4;
+        private System.Windows.Forms.Label number4;
+        private System.Windows.Forms.Label text5;
+        private System.Windows.Forms.Label number5;
+        private System.Windows.Forms.Label text6;
+        private System.Windows.Forms.Label number6;
+        private System.Windows.Forms.Button buttonGenerate;
+    }
+}
+
