683e83d748968225de72baf24914a7540b5b8561
Niagara Georgieva <niagara.georgieva@abv.bg>
2022-12-17 23:11:29 +0200

    Add files via upload

---
+++ 11th grade/Module 1 - Object-oriented design and programming/Visual programming/20.01.2021/Toto2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toto2
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int GetRandom() { return r.Next(1, 50); }
+
+        bool InArray(int[] arr, int n)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i]==n) { return true; }
+            }
+            return false;
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            int n;
+            int[] numbers = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                do { n = GetRandom(); } while (InArray(numbers, n));
+                numbers[i] = n;
+            }
+            labelNumbers.Text = string.Join("; ", numbers);
+        }
+
+        private void buttonClean_Click(object sender, EventArgs e) { labelNumbers.Text = ""; }
+    }
+}
