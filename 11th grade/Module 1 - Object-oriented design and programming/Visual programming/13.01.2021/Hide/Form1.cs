bd7910b810e7fa055b3bc500b20491d106b50a8c
Niagara Georgieva <niagara.georgieva@abv.bg>
2023-01-18 23:34:57 +0200

    Add files via upload

---
+++ 11th grade/Module 1 - Object-oriented design and programming/Visual programming/13.01.2021/Hide/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hide
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Visible = true;
+            textBoxResult.Text = "Hello World";
+        }
+
+        private void buttonHide_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Visible = false;
+        }
+    }
+}
