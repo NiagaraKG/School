1b457ebe269ed632cb659feae798810d79b40a2e
Niagara Georgieva <niagara.georgieva@abv.bg>
2023-02-17 23:32:47 +0200

    Add files via upload

---
+++ 11th grade/Module 1 - Object-oriented design and programming/Console programming/Apples/CaseB/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CaseB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int g = 2000;
+            double tons = 20.00, sum = 0;
+            while (sum <= 900)
+            {               
+                if (g % 2 == 0 && g != 2000) { tons *= 1.2; }
+                sum += tons;
+                g++;
+            }
+            g--;
+            Console.WriteLine(g);
+        }
+    }
+}
