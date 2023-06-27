8e83d7cd789ec7650a3469f9ef4fbf2a73ed236a
Niagara Georgieva <niagara.georgieva@gmail.com>
2023-06-27 23:47:08 +0300

    Delete Lab02Problem02-Niagara-Georgieva.py

--- Lab02Problem02-Niagara-Georgieva.py
+++
@@ -1,6 +0,0 @@
-course_name = input("Enter a course name: ")
-course_number = input("Enter a course number: ")
-course_section = input("Enter a course section: ")
-semester = input("Enter a semester (Winter, Summer): ")
-year = int(input("Enter a year: "))
-print("\nThe course code is {:.3}{}{}{:.1}{}".format(course_name, course_number, course_section, semester, year))
