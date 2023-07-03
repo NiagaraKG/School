4dd39dfaeb73bf0a3e72be2d08b9000efe95fd8f
Niagara Georgieva <niagara.georgieva@gmail.com>
2023-07-03 23:29:38 +0300

    Delete Lab05Problem01-Niagara-Georgieva.py

--- Lab05Problem01-Niagara-Georgieva.py
+++
@@ -1,9 +0,0 @@
-start = int(input("Enter a low value: "))
-end = int(input("Enter a high value: "))
-step = int(input("Enter a step value: "))
-print()
-print(" " * 4, "Pounds", " " * 4, "Ounces"," " * 4,"Grams")
-for pounds in range(start, end + 1, step):
-    ounces = pounds * 16.0
-    grams = pounds * 453.592
-    print("{:11.1f} {:11.1f} {:11.1f}".format(pounds, ounces, grams))
