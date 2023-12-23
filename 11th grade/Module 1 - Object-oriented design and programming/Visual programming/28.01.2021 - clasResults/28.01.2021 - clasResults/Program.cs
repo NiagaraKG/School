bcc8a9a1d53a1fb1e59130f088c308a4e8dec65d
Niagara Georgieva <niagara.georgieva@gmail.com>
2023-12-23 22:59:15 +0200

    Update Program.cs

--- 11th grade/Module 1 - Object-oriented design and programming/Visual programming/28.01.2021 - clasResults/28.01.2021 - clasResults/Program.cs
+++ 11th grade/Module 1 - Object-oriented design and programming/Visual programming/28.01.2021 - clasResults/28.01.2021 - clasResults/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace _28._01._2021___clasResults
+namespace _28._01clasResults
 {
     static class Program
     {
